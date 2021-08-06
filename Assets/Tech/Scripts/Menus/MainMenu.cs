3010edd5c36e5a551a640e5ead8942e5eca6de2e
Elie667 <elie.bouillard@lacatholille.fr>
2021-08-06 17:41:24 +0200

    [ADD] - Steamwork Transport

--- Assets/Tech/Scripts/Menus/MainMenu.cs
+++ Assets/Tech/Scripts/Menus/MainMenu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
-//using Steamworks;
+using Steamworks;
 
 public class MainMenu : MonoBehaviour
 {
@@ -10,56 +10,57 @@ public class MainMenu : MonoBehaviour
 
     [SerializeField] private bool useSteam = false;
 
-    //protected Callback<LobbyCreated_t> lobbyCreated;
-    //protected Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
-    //protected Callback<LobbyEnter_t> lobbyEntered;
+    protected Callback<LobbyCreated_t> lobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
+    protected Callback<LobbyEnter_t> lobbyEntered;
 
     private void Start()
     {
         if (!useSteam) { return; }
 
-        //lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
-        //gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested);
-        //lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+        lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+        gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested);
+        lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
     }
 
     public void HostLobby()
     {
         landingPagePanel.SetActive(false);
 
-        //if (useSteam)
-        //{
-        //    SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4);
-        //    return;
-        //}
+        if (useSteam)
+        {
+            SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4);
+            return;
+        }
 
         NetworkManager.singleton.StartHost();
     }
 
-    //private void OnLobbyCreated(LobbyCreated_t callback)
-    //{
-    //    if (callback.m_eResult != EResult.k_EResultOK)
-    //    {
-    //        landingPagePanel.SetActive(true);
-    //        return;
-    //    }
+    private void OnLobbyCreated(LobbyCreated_t callback)
+    {
+        if (callback.m_eResult != EResult.k_EResultOK)
+        {
+            landingPagePanel.SetActive(true);
+            return;
+        }
 
-    //    NetworkManager.singleton.StartHost();
+        NetworkManager.singleton.StartHost();
 
-    //    SteamMatchmaking.SetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), "HostAdress", SteamUser.GetSteamID().ToString());
-    //}
-    //private void OnGameLobbyJoinRequested(GameLobbyJoinRequested_t callback)
-    //{
-    //    SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
-    //}
+        SteamMatchmaking.SetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), "HostAdress", SteamUser.GetSteamID().ToString());
+    }
 
-    //private void OnLobbyEntered(LobbyEnter_t callback)
-    //{
-    //    if (NetworkServer.active) { return; }
+    private void OnGameLobbyJoinRequested(GameLobbyJoinRequested_t callback)
+    {
+        SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+    }
 
-    //    string hostAdress = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), "HostAdress");
-    //    NetworkManager.singleton.networkAddress = hostAdress;
-    //    NetworkManager.singleton.StartClient();
-    //    landingPagePanel.SetActive(false);
-    //}
+    private void OnLobbyEntered(LobbyEnter_t callback)
+    {
+        if (NetworkServer.active) { return; }
+
+        string hostAdress = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), "HostAdress");
+        NetworkManager.singleton.networkAddress = hostAdress;
+        NetworkManager.singleton.StartClient();
+        landingPagePanel.SetActive(false);
+    }
 }
